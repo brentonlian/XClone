46acb06051c0710cf1631d22edf617695dc3a7ad
Brenton Lian <brenton.lian@gmail.com>
2024-12-23 21:58:38 -0500

    Half finished button styling

--- app/login/github-button.tsx
+++ app/login/github-button.tsx
@@ -4,7 +4,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
 
 export default function GitHubButton() {
-  const supabase = createClientComponentClient<Database>();
+  const supabase = createClientComponentClient();
 
   const handleSignIn = async () => {
     await supabase.auth.signInWithOAuth({
@@ -16,13 +16,19 @@ export default function GitHubButton() {
   };
 
   return (
-    <button onClick={handleSignIn} className="hover:bg-gray-800 p-8 rounded-xl">
-      <Image
-        src="/loginWithGithub.png"
-        alt="GitHub logo"
-        width={100}
-        height={100}
-      />
-    </button>
+    <div className="flex items-center justify-center min-h-screen w-full bg-white">
+      <button
+        onClick={handleSignIn}
+        className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 px-6 py-3 rounded-lg shadow-lg transition duration-300"
+      >
+        <Image
+          src="/github-mark-white.png"
+          alt="GitHub logo"
+          width={40}
+          height={40}
+        />
+        <span className="ml-2 text-white font-semibold">Login with GitHub</span>
+      </button>
+    </div>
   );
 }
